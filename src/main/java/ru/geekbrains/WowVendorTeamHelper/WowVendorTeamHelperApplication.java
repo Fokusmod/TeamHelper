d29c796ef6a9c00b4c000d06710d277082f7e964
Fokusmod <Info@mail.ru>
2023-01-26 02:21:41 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains/WowVendorTeamHelper/WowVendorTeamHelperApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.WowVendorTeamHelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WowVendorTeamHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WowVendorTeamHelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/geekbrains/WowVendorTeamHelper/WowVendorTeamHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.WowVendorTeamHelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WowVendorTeamHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
