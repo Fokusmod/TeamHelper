4e72f29e33e531b862f09d519d16e2787b5b827c
Timofey <timal1@yandex.ru>
2023-01-31 00:05:05 +0300

    1) Добавил авторизацию, пока простую без регистрации 2) добавил фаил secrets.properties и вынес туда все что должно быть скрыто

---
+++ src/main/java/ru/geekbrains/WowVendorTeamHelper/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package ru.geekbrains.WowVendorTeamHelper.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.geekbrains.WowVendorTeamHelper.model.Role;
+
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
