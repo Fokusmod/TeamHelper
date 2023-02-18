b1d1c1000430a9a547299fa3d292cf4f7b5fa307
tanisha <tanyshkafr@mail.ru>
2023-02-19 01:40:53 +0300

    schedule_page

--- src/main/resources/static/admin/admin.js
+++ src/main/resources/static/admin/admin.js
@@ -1,7 +1,6 @@
 angular.module('index-app').controller('adminController', function ($scope, $http, $location, $localStorage, $rootScope) {
 const contextPath = 'http://localhost:3100';
 
-
     $rootScope.isUserLoggedIn = function () {
         if ($localStorage.springWebUser) {
             return true;
@@ -11,7 +10,6 @@ const contextPath = 'http://localhost:3100';
         }
     };
 
-
     $scope.loadUsers = function () {
         $http.get(contextPath + '/api/v1/user')
         .then(function (response) {
@@ -19,38 +17,38 @@ const contextPath = 'http://localhost:3100';
         });
     };
 
-
     $scope.loadNotApprovedUsers = function () {
      var user_status ='not_approved';
         $http({
-            url: contextPath + '/api/v1/user/status',
+            url: contextPath + 'api/v1/user/status',
             method: 'GET',
             params: {
                 status: user_status
             }
         }).then(function (response) {
+        console.log(data);
             $scope.notApprovedUsersList = response.data;
         });
     };
 
 
-    $scope.deleteUser = function (userId) {
+/*    $scope.deleteUser = function (userId) {
         $http.post(contextPath + '/api/v1/user/delete/' + userId)
         .then(function (response) {
             $scope.loadUsers();
             $scope.loadNotApprovedUsers();
         });
-    };
+    };*/
 
     $scope.approveUser = function (userId) {
-        $http.post(contextPath + '/user_approved/' + userId)
+        $http.post(contextPath + '/api/v1/user/' + userId + '/status/' + 1)
         .then(function (response) {
             $scope.loadUsers();
-            $scope.loadNotApprovedUsers();
+/*            $scope.loadNotApprovedUsers();*/
         });
     };
 
-    $scope.getRoles = function () {
+/*    $scope.getRoles = function () {
         $http.get("http://localhost:3100/api/v1/role")
             .then(function successCallback(response) {
                 $scope.roles = response.data;
@@ -58,9 +56,9 @@ const contextPath = 'http://localhost:3100';
             }, function failCallback(response) {
 
             })
-    };
+    };*/
 
-     $scope.changeRole = function (userID) {
+/*     $scope.changeRole = function (userID) {
             let selectedRole = document.getElementById('role-selected').value;
             console.log(selectedRole);
             $http.put(contextPath + '/api/v1/user/' + userID + '/change_role/' + selectedRole)
@@ -68,10 +66,10 @@ const contextPath = 'http://localhost:3100';
 
                 }, function failCallback(response) {
             })
-     }
+     }*/
 
     $scope.loadUsers();
     $scope.loadNotApprovedUsers();
-    $scope.getRoles();
+/*    $scope.getRoles();*/
 
 });
