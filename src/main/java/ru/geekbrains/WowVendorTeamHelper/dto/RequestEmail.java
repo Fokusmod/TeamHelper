a26302321c24a0403fc22282de45e27451750701
Timofey <timal1@yandex.ru>
2023-02-19 22:14:24 +0300

    Added DTO for change password

---
+++ src/main/java/ru/geekbrains/WowVendorTeamHelper/dto/RequestEmail.java
@@ -0,0 +1,12 @@
+package ru.geekbrains.WowVendorTeamHelper.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RequestEmail {
+    private String email;
+}
